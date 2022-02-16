2383387b4a6598e0f45c1b84ad626bba006e759a
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-02-16 09:53:31 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/ordershop/OrderShopApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.ordershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/ordershop/OrderShopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.ordershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
